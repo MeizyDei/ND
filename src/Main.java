6c168f00505cf146f44df6379085a72e5f76c6f4
MeizyDei <115172495+MeizyDei@users.noreply.github.com>
2022-10-25 21:54:43 +0300

    sukurti du metodai. arSkaiciusYraSarase(is meniu pasirinkimu) ir skPasirinkimas tikriname ar ivestas skaicius, ar skaicius ivestas leistinose reiksmese.

--- src/Main.java
+++ src/Main.java
@@ -22,15 +22,15 @@ public class Main {
         numeriais (sakykime įvyko numerio nuskaitymo klaida) turi būti spausdinamas
         klaidos pranešimas.
          */
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         ArrayList<String> atvykstantysAutomobiliai = new ArrayList<>();
         ArrayList<String> isvykstantysAutomobiliai = new ArrayList<>();
 
         System.out.println("Parkavimo aiksteles automobiliu registravimo programa.");
-        System.out.println("iveskite Automobilio numeri");
-        while (sc.hasNext()) {
-            String numeris = gautiAutomobilioNumeri(sc);
+//        System.out.println("iveskite Automobilio numeri");
+        String numeris = gautiAutomobilioNumeri(scanner);
+        boolean testiPrograma = true;
             if (numeris.substring(0, 2).equals("in")) {
                 atvykstantysAutomobiliai.add(numeris);
             } else if (numeris.substring(0, 3).equals("out")) {
@@ -38,14 +38,43 @@ public class Main {
             } else {
                 System.out.println("Klaida! Pries numerio kombinacija iveskite in/out");
             }
-        }
-
-
     }
-
-    private static String gautiAutomobilioNumeri(Scanner sc) {
+    public static int meniu(Scanner scanner){
+        System.out.println("[1] - Įvažiavimas\n" +
+                            "[2] - Išvažiavimas\n");
+        return 0;
+    }
+    public static int skPasirinkimas(Scanner scanner, int[] leistinosReiksmes){
+        int pasirinkimas = 0;
+        boolean neivesta = true;
+        while(neivesta){
+            String ivestiDuomenys = scanner.next();
+            try{
+                pasirinkimas = Integer.parseInt(ivestiDuomenys);
+                if(leistinosReiksmes.length == 0 || arSkaiciusYraSarase(leistinosReiksmes, pasirinkimas)){
+                    neivesta = false;
+                }else{
+                    System.out.println("Nezinoma komanda");
+                }
+            }catch(NumberFormatException nfe){
+                System.out.println("Klaida!");
+            }
+        }
+        return pasirinkimas;
+    }
+    public static boolean arSkaiciusYraSarase (final int[] skaiciuSarasas, final int skaicius){
+        boolean result = false;
+        for(int i : skaiciuSarasas){
+            if(i == skaicius){
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+    private static String gautiAutomobilioNumeri(Scanner scanner) {
         System.out.println("iveskit auto numeri");
-        String autoNumeris = sc.nextLine();
+        String autoNumeris = scanner.nextLine();
         return autoNumeris;
     }
 
